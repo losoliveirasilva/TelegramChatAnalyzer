4b72d425b77468d8e15f59b9a488e6ecdf09b5ed
Leonardo de Oliveira da Silva <los.oliveirasilva@gmail.com>
2018-10-19 22:22:07 -0300

    Add voice counter

--- index.js
+++ index.js
@@ -20,10 +20,26 @@ const messagesPerAuthor = peopleMsg => {
 const countWords = peopleMsg => {
 	let counter = new Map()
 	for (let {text} of peopleMsg.filter(msg => msg.type === 'text')) {
-		const words = text.normalize('NFKD').toLowerCase().match(/[a-z0-9]+/g) || []
+		const words = text.normalize('NFKD').toLowerCase().split(/([[:punct:]]|\s)+/) || []
 		for (let word of words) {
 			counter.set(word, (counter.get(word) || 0) + 1)
 		}
+	}
+	return counter
+}
+
+const convertDurationToSeconds = duration => {
+	durationArr = duration.split(",")
+	durationArr = durationArr[0].split(":")
+	return (+durationArr[0] * 60) + +durationArr[1]
+}
+
+const countVoice = peopleMsg => {
+	let counter = new Map()
+	for (const {author, duration} of peopleMsg.filter(msg => msg.type === 'voice')) {
+		const [a, b] = counter.get(author) || [0, 0]
+		counter.set(author, [a + 1, b + convertDurationToSeconds(duration)])
+		console.log(duration)
 	}
 	return counter
 }
@@ -47,9 +63,73 @@ document.addEventListener('DOMContentLoaded', () => {
 		const callback = waitForCalls(fileInput.files.length, () => {
 			console.log(messagesPerAuthor(peopleMsg))
 
-			console.log(Array.from(countWords(peopleMsg).entries()).sort((a, b) => {
+			let wordsSort = Array.from(countWords(peopleMsg).entries()).sort((a, b) => {
 				return a[1] < b[1]
-			}))
+			})
+
+			let authorMsgSort = Array.from(messagesPerAuthor(peopleMsg).entries()).sort((a, b) => {
+				return a[1] < b[1]
+			})
+
+			let authorVoiceSort = Array.from(countVoice(peopleMsg).entries()).sort((a, b) => {
+				return a[1] < b[1]
+			})
+
+			let body = document.getElementsByTagName("body")[0]
+
+			let tbl = document.createElement("table")
+			let tblBody = document.createElement("tbody")
+
+			for (let tableAuthor of authorVoiceSort) {
+				let row = document.createElement("tr")
+
+				tblBody.appendChild(row)
+
+				let cellA = document.createElement("td");
+				let cellAText = document.createTextNode(tableAuthor[0]);
+				cellA.appendChild(cellAText);
+				row.appendChild(cellA);
+
+				let cellN = document.createElement("td");
+				let cellNText = document.createTextNode(tableAuthor[1]);
+				cellN.appendChild(cellNText);
+				row.appendChild(cellN);
+			}
+
+			let tbl2 = document.createElement("table")
+			let tblBody2 = document.createElement("tbody")
+
+			let counter = 0
+			for (let tableMsg of wordsSort) {
+				if (counter === 2000)
+					continue
+				else
+					counter += 1
+
+				let row = document.createElement("tr")
+
+				tblBody2.appendChild(row)
+
+				let cellA = document.createElement("td");
+				let cellAText = document.createTextNode(tableMsg[0]);
+				cellA.appendChild(cellAText);
+				row.appendChild(cellA);
+
+				let cellN = document.createElement("td");
+				let cellNText = document.createTextNode(tableMsg[1]);
+				cellN.appendChild(cellNText);
+				row.appendChild(cellN);
+			}
+
+
+			tbl.appendChild(tblBody)
+			tbl2.appendChild(tblBody2)
+			let br = document.createElement("br")
+			body.appendChild(tbl)
+			body.appendChild(br)
+			body.appendChild(tbl2)
+			tbl.setAttribute("border", "2")
+			tbl2.setAttribute("border", "1")
 		})
 
 		for (let file of fileInput.files) {
@@ -72,17 +152,20 @@ document.addEventListener('DOMContentLoaded', () => {
 					const id = +message.id.substr(7)  // cut off 'message'
 
 					const metadata = {id, author: name}
-					const msgMedia = message.querySelector('.media')
+					const msgMediaVoiceMessage = message.querySelector('.media_voice_message')
+					const msgMediaPhoto = message.querySelector('.media')
 					const msgText = message.querySelector('.text')
-					if (msgMedia !== null) {
-						metadata.type = 'media'
-						metadata.description = msgMedia.querySelector('.details').innerText.trim()
-						metadata.title = msgMedia.querySelector('.title').innerText.trim()
+					if (msgMediaVoiceMessage !== null) {
+						metadata.type = 'voice'
+						metadata.duration = msgMediaVoiceMessage.querySelector('.details').innerText.trim()
+						metadata.title = msgMediaVoiceMessage.querySelector('.title').innerText.trim()
+					} else if (msgMediaPhoto !== null) {
+						metadata.type = 'photo'
 					} else if (msgText !== null) {
 						metadata.type = 'text'
 						metadata.text = msgText.innerText.trim()
 					} else {
-						console.warning('Unrecognized message:', message)
+						console.log('Unrecognized message:', message)
 					}
 
 					const msgReplyTo = message.querySelector('.reply_to')
